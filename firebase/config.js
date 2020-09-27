e6dbd8196f63c7b2fd17a2d35cdea820a6b3f801
dcamigira11 <dcamigira11@gmail.com>
2020-09-27 15:46:42 -0500

    HuntNext

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCK8VC_fSnAQAOZ87OXZglrnHcIlRV4MKo",
+    authDomain: "huntnext-fb590.firebaseapp.com",
+    databaseURL: "https://huntnext-fb590.firebaseio.com",
+    projectId: "huntnext-fb590",
+    storageBucket: "huntnext-fb590.appspot.com",
+    messagingSenderId: "351378973493",
+    appId: "1:351378973493:web:1f97ee7713f4af29c3227c"
+  };
+
+export default firebaseConfig;
